82c1246138605872bc823f1ae1916b62e92e6fbf
unknown <zgonzales16@gmail.com>
2021-10-12 21:49:27 -0500

    final querie from module 7

--- queries/queries.sql
+++ queries/queries.sql
@@ -189,3 +189,82 @@ LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no
+
+SELECT * FROM salaries;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+
+-- create emp_info table
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	   AND (de.to_date = '9999-01-01');
+	   
+	   
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM managers AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+select * from manager_info
+
+-- create dept_info table
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- create sales team info
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	dpt.dept_name
+INTO sales_team_retirement_info
+FROM retirement_info as ri
+	inner join dept_emp as de
+		on (ri.emp_no = de.emp_no)
+	inner join departments as dpt
+		on (de.dept_no = dpt.dept_no)
+where dept_name = 'Sales';
+
+-- get sales and development info
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	dpt.dept_name
+INTO sales_and_development
+FROM retirement_info as ri
+	inner join dept_emp as de
+		on (ri.emp_no = de.emp_no)
+	inner join departments as dpt
+		on (de.dept_no = dpt.dept_no)
+where dept_name in ('Sales', 'Development');   
