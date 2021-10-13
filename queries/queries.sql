ce3b30a43c88a27c84177938fe302d4178fed69d
unknown <zgonzales16@gmail.com>
2021-10-12 20:21:52 -0500

    adding queries

---
+++ queries/queries.sql
@@ -0,0 +1,113 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE (dept_name)
+);
+create table dept_emp (
+	emp_no int not null,
+	dept_no varchar not null,
+	from_date date not null,
+	to_date date not null,
+	foreign key (emp_no) references employees (emp_no),
+	foreign key (dept_no) references departments (dept_no),
+	primary key (emp_no, dept_no)
+);
+create table employees (
+	emp_no int not null,
+	birth_date date not null,
+	first_name varchar not null,
+	last_name varchar not null,
+	gender varchar not null,
+	hire_date date not null,
+	primary key (emp_no)
+);
+create table managers (
+	dept_no varchar not null,
+	emp_no int not null,
+	from_date date not null,
+	to_date date not null,
+	foreign key (emp_no) references employees (emp_no),
+	foreign key (dept_no) references departments (dept_no),
+	primary key (emp_no, dept_no)
+);
+create table salaries (
+	emp_no int not null,
+	salary int not null,
+	from_date date not null,
+	to_date date not null,
+	foreign key (emp_no) references employees (emp_no),
+	primary key (emp_no)
+);
+create table titles (
+	emp_no int not null,
+	title varchar not null,
+	from_date date not null,
+	to_date date not null,
+	foreign key (emp_no) references employees (emp_no),
+	primary key (emp_no, from_date)
+);
+
+select * from departments;
+select * from dept_emp;
+select * from employees;
+select * from managers;
+select * from salaries;
+select * from titles;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Creating retirement info table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- drop table due to mistake
+DROP TABLE retirement_info CASCADE;
+
+-- try again
+-- Creating retirement info table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+SELECT * FROM retirement_info;
