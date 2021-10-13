bea20d26e8086a710319de87f8bdd725bd5e5ea9
unknown <zgonzales16@gmail.com>
2021-10-12 21:17:26 -0500

    updating queries 7.34

--- queries/queries.sql
+++ queries/queries.sql
@@ -111,3 +111,81 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 
 SELECT * FROM retirement_info;
+
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     managers.emp_no,
+     managers.from_date,
+     managers.to_date
+FROM departments
+INNER JOIN managers
+ON departments.dept_no = managers.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+	FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+
+SELECT d.dept_name,
+     m.emp_no,
+     m.from_date,
+     m.to_date
+FROM departments as d
+INNER JOIN managers as m
+ON d.dept_no = m.dept_no;
+
+
+-- joining retirement info and dept info to see who is still employed
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+select * from current_emp
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- create count by department number table
+SELECT COUNT (ce.emp_no), de.dept_no
+INTO dept_no_count
+from current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no
