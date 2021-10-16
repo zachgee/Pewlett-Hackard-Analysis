ac283ee45b50a0943e2c8958a48c49cb8bec1376
unknown <zgonzales16@gmail.com>
2021-10-15 22:27:41 -0500

    adding code from deliverables one and two

--- queries/Employee_Database_challenge.sql
+++ queries/Employee_Database_challenge.sql
@@ -42,14 +42,14 @@ e.last_name,
 e.birth_date,
 de.from_date,
 de.to_date,
-t.title
---DISTINCT ON(emp_no)
---INTO mentor_eligibility
+tt.title
+
+INTO mentor_eligibility
 FROM employees AS e
 INNER JOIN dept_emp AS de
 ON (e.emp_no = de.emp_no)
-INNER JOIN titles AS t
-ON (e.emp_no = t.emp_no)
+INNER JOIN titles AS tt
+ON (e.emp_no = tt.emp_no)
 WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
 ORDER BY emp_no;
